4f02c3fc15c7b86d9453b9b08cde144ff3510f95
hg <bhg5460@gmail.com>
2022-01-23 16:50:27 +0900

    first commit
    Signed-off-by: hg <bhg5460@gmail.com>

---
+++ src/main/java/com/example/hg/HgApplication.java
@@ -0,0 +1,13 @@
+package com.example.hg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HgApplication.class, args);
+    }
+
+}
